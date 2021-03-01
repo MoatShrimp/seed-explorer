7e7c999ff09e45bd307fa14c98a31c35dde94801
MoatShrimp <mathias.viklund@gmail.com>
2021-03-01 09:33:45 +0100

    Updated with better variable names

--- ts/floor-generation/dibble.ts
+++ ts/floor-generation/dibble.ts
@@ -1,17 +1,8 @@
-//remove items from unchecked check-boxes from the lootTable
-/*function toggleMissing () {
-	const unckeckedBoxes = Object.values(settings.checkboxes.relic).filter((box) => !box.checked);
-	const unckeckedPotions = Object.values(settings.checkboxes.potion).filter((box) => !box.checked);
-	const missing = unckeckedBoxes.map((box) => box.value);
-	const missingPotions = unckeckedPotions.map((box) => box.value);
-	toggleWeight(missing);
-	toggleWeight(missingPotions, 'potion');
 
-}*/
 //load Dibbles stock
 function dibble(seed) {
 
-	let table = [], index = [], item = [], html = [], wTable = [];
+	let tables = [], index = [], item = [], html = [], wTables = [];
 	let field = document.createElement("fieldset");
 	field.id = 'hub';
 	field.classList.add('zone');
@@ -28,27 +19,27 @@ function dibble(seed) {
 	subfield.appendChild(sublegend);
 	e$('#hub').appendChild(subfield);
 
-	wTable.push(rand.shop.getWeightedTable(lootTables.dibbleRelic));
-	wTable.push(rand.shop.getWeightedTable(lootTables.dibble));
-	wTable.push(rand.shop.getWeightedTable(lootTables.dibble));
+	wTables.push(rand.shop.getWeightedTable(lootTables.dibbleRelic));
+	wTables.push(rand.shop.getWeightedTable(lootTables.dibble));
+	wTables.push(rand.shop.getWeightedTable(lootTables.dibble));
 
-	wTable.forEach((wTable) => {
-		table.push(masterTable.weightedTables[wTable.masterIndex]);
+	wTables.forEach((wTable) => {
+		tables.push(masterTable.weightedTables[wTable.masterIndex]);
 	});
 
-	table.forEach((table) => {
+	tables.forEach((table) => {
 		index.push(rand[table.randState].loot(lootTables[table.key]));
 	});
 
 	index.forEach((index, i) => {
-		item.push(masterTable[table[i].type][index]);
+		item.push(masterTable[tables[i].type][index]);
 	});
 
-	if (table[0].type == 'relic') {
+	if (tables[0].type == 'relic') {
 		toggleWeight(index[0]);
 	}
 
-	table.forEach((table, i) => {
+	tables.forEach((table, i) => {
 		html.push(document.createElement("div"));
 		html[i].classList.add('icon-' + table.type);
 		html[i].innerHTML = item[i].display;
